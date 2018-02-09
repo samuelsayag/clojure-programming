4fabde2674c66b176f423df1880dc45a52f9c424
Samuel SAYAG <samouille666@hotmail.com>
2018-02-09 14:39:05 +0200

    keyword example

---
+++ chp1/keywords.clj
@@ -0,0 +1,10 @@
+( def person  { :name "Santa Cruz"
+		:city "Portland, ME"})
+
+(def pizza {:name "Ramunto's"
+	:location "Claremont, NH"
+	::location "43.3734,-72.3365"})
+
+(def mysymbol {
+	:?!strange "this ?! is a strange symbol"
+})
