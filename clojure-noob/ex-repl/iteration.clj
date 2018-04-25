6cee7979b8001177bcd150d0cb3b1edde7b5dc9d
Samuel SAYAG <samuel.sayag@clicktale.com>
2018-04-25 08:04:49 +0300

    liora work

---
+++ clojure-noob/ex-repl/iteration.clj
@@ -0,0 +1,8 @@
+(defn recursive-printer
+  ([]
+   (recursive-printer 0))
+  ([iteration]
+   (println iteration)
+   (if (> iteration 3)
+     (println "Goodbye!")
+     (recursive-printer (inc iteration)))))
---
+++ clojure-noob/ex-repl/liora-do.clj
@@ -0,0 +1,17 @@
+(if
+  (> (+ 6 5)
+     (+ 3 4))
+  (do (println "Good!")
+               "You did it right")
+  (do (println "NO!!!"
+               "WHAT DID YOU DO???")))
+
+(if 
+  (> (+ 6 5)
+     (+ 10 4))
+  (do (println "Good!")
+               "You did it right")
+  (do (println "NO!!!"
+               "WHAT DID YOU DO???")))
+
+
